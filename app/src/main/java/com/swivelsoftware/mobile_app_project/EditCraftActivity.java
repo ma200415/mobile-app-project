021edecd5529bc1fa5b96c448e90888f8598b640
Vincent Michael <ma20200415@gmail.com>
2022-05-24 14:08:20 +0800

    show datepicker

--- app/src/main/java/com/swivelsoftware/mobile_app_project/EditCraftActivity.java
+++ app/src/main/java/com/swivelsoftware/mobile_app_project/EditCraftActivity.java
@@ -1,6 +1,5 @@
 package com.swivelsoftware.mobile_app_project;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,32 +8,35 @@ import android.widget.AutoCompleteTextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.util.Pair;
-import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.datepicker.MaterialDatePicker;
-import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 import com.swivelsoftware.mobile_app_project.databinding.ActivityEditCraftBinding;
 
 import java.util.Calendar;
 
 public class EditCraftActivity extends AppCompatActivity {
+    ActivityEditCraftBinding binding;
+
+    final String[] stores = new String[]{"Wong Tai Sin", "Tsuen Wan", "Causeway Bay", "Mong Kok"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ActivityEditCraftBinding binding = ActivityEditCraftBinding.inflate(getLayoutInflater());
+        binding = ActivityEditCraftBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        String[] items = new String[]{"Option 1", "Option 2", "Option 3", "Option 4"};
-        ArrayAdapter sss = new ArrayAdapter(this, R.layout.craft_store_list_item, items);
+        ArrayAdapter sss = new ArrayAdapter(this, R.layout.craft_store_list_item, stores);
 
         AutoCompleteTextView sssss = (AutoCompleteTextView) binding.store.getEditText();
 
         sssss.setAdapter(sss);
+
+        binding.date.setEndIconOnClickListener(event -> showDatePicker());
     }
 
-    public void showDatePicker(View view) {
+    public void showDatePicker() {
         MaterialDatePicker dateRangePicker =
                 MaterialDatePicker.Builder.dateRangePicker()
                         .setTitleText("Select dates")
@@ -42,10 +44,6 @@ public class EditCraftActivity extends AppCompatActivity {
 
         dateRangePicker.addOnPositiveButtonClickListener(selection -> {
             Pair result = (Pair) selection;
-//                fromCal.setTimeInMillis((long)result.first);
-//                item.setStartDate(fromCal);
-//                toCal.setTimeInMillis((long)result.second);
-//                item.setEndDate(toCal);
 
             Calendar fromCal = Calendar.getInstance();
             Calendar toCal = Calendar.getInstance();
@@ -55,10 +53,10 @@ public class EditCraftActivity extends AppCompatActivity {
 
             Log.d("+++++", fromCal.getTime() + "");
             Log.d("+++++", toCal.getTime() + "");
+
+            binding.date.getEditText().setText(fromCal.getTime().toString());
         });
 
         dateRangePicker.show(getSupportFragmentManager(), "dateRangePicker");
-
-
     }
 }
